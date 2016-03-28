8b54654839d643ddc0f915e71f207a93c12b1d92
Jigar Gosalia <jgosalia@paypal.com>
2016-03-28 02:12:00 -0700

    LAZY: Update Bubble Sort

--- src/com/programs/sorting/OptimizedBubbleSort.java
+++ src/com/programs/sorting/OptimizedBubbleSort.java
@@ -3,7 +3,12 @@ package com.programs.sorting;
 import java.util.Arrays;
 
 /**
- * Bubble sort is an in-place sorting algorithm. <br>
+ * Bubble sort is an in-place sorting algorithm.<br>
+ * <br>
+ * 
+ * Bubble Sort can be optimized by adding a swapping flag which is set when
+ * swapping is done in inner loop and it can be checked after every inner loop
+ * execution so that if no elements are swapped then it means array is sorted.<br>
  * <br>
  * 
  * This sorting algorithm is comparison based algorithm in which each pair of
@@ -16,7 +21,7 @@ import java.util.Arrays;
  * @author "Jigar Gosalia"
  *
  */
-public class BubbleSort {
+public class OptimizedBubbleSort {
 
 	/**
 	 * @param args
@@ -36,7 +41,9 @@ public class BubbleSort {
 	 */
 	public static <T extends Comparable<T>> void bubbleSort(final T[] inputArray) {
 		int iterations = 0;
+		boolean swapped = false;
 		for (int i = 0; i < inputArray.length; i++) {
+			swapped = false;
 			for (int j = 0; j < inputArray.length - 1; j++) {
 				iterations++;
 				// Swap only if element is greater then next element.
@@ -45,7 +52,13 @@ public class BubbleSort {
 					T temp = inputArray[j + 1];
 					inputArray[j + 1] = inputArray[j];
 					inputArray[j] = temp;
+					swapped = true;
 				}
+			}
+			// Check swapped flag after each inner iteration, if none of the
+			// element is swapped then this means that the array is sorted.
+			if (!swapped) {
+				break;
 			}
 		}
 		System.out.println("Iterations:<" + iterations + "> Size:<" + inputArray.length + ">");
