ac2285cdc9fd6ebf698f884d277ad31a0f45ed3f
Jigar Gosalia <jgosalia@paypal.com>
2016-04-04 22:10:23 -0700

    LAZY: Update String interning code

--- src/com/programs/string/StringIntern.java
+++ src/com/programs/string/StringIntern.java
@@ -1,6 +1,38 @@
 package com.programs.string;
 
 /**
+ * <b>String automatically calls intern() on literal constants so technically,
+ * if required, it should be only called on new String().</b> <br>
+ * <br>
+ * String Interning is a method of storing only one copy of each distinct String
+ * Value, which must be immutable.<br>
+ * <br>
+ * In Java, String class has a public method intern() that returns a canonical
+ * representation for the string object. Java’s String class privately maintains
+ * a pool of strings, where String literals are automatically interned.<br>
+ * <br>
+ * When the intern() method is invoked on a String object it looks the string
+ * contained by this String object in the pool, if the string is found there
+ * then the string from the pool is returned. Otherwise, this String object is
+ * added to the pool and a reference to this String object is returned.<br>
+ * <br>
+ * The intern() method helps in comparing two String objects with == operator by
+ * looking into the pre-existing pool of string literals, no doubt it is faster
+ * than equals() method. The pool of strings in Java is maintained for saving
+ * space and for faster comparisons. Normally Java programmers are advised to
+ * use equals(), not ==, to compare two strings. This is because == operator
+ * compares memory locations, while equals() method compares the content stored
+ * in two objects.<br>
+ * <br>
+ * <b>Why and When to Intern ?</b><br>
+ * <br>
+ * Though Java automatically interns all Strings by default, remember that we
+ * only need to intern strings when they are not constants, and we want to be
+ * able to quickly compare them to other interned strings. The intern() method
+ * should be used on strings constructed with new String() in order to compare
+ * them by == operator. <br>
+ * <br>
+ * 
  * @author "Jigar Gosalia"
  *
  */
@@ -10,19 +42,19 @@ public class StringIntern {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String interned = new String(new char[] { 'i', 'n', 't' }).intern();
-		String notInterned = new String(new char[] { 'n', 'o', 't' });
-		System.out.println("Case 1: " + isInterned(interned));
-		System.out.println("Case 2: " + isInterned(new String(interned)));
-		System.out.println("Case 3: " + isInterned(notInterned));
+		String s1 = "Test";
+		String s2 = "Test";
+		String s3 = new String("Test");
+		final String s4 = s3.intern();
+		System.out.println(s1 == s2);
+		System.out.println(s2 == s3);
+		System.out.println(s3 == s4);
+		System.out.println(s1 == s3);
+		System.out.println(s1 == s4);
+		System.out.println(s1.equals(s2));
+		System.out.println(s2.equals(s3));
+		System.out.println(s3.equals(s4));
+		System.out.println(s1.equals(s4));
+		System.out.println(s1.equals(s3));
 	}
-
-	/**
-	 * @param s
-	 * @return
-	 */
-	public static boolean isInterned(String s) {
-		return (new String(s)).intern() == s;
-	}
-
 }
