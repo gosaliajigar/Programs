1d3f303e16583a6b84b123bc650ee1e62643a69a
Jigar Gosalia <jgosalia@paypal.com>
2017-08-31 23:30:55 -0700

    LAZY: add comments

--- src/string/StringPermutationRecursive.java
+++ src/string/StringPermutationRecursive.java
@@ -35,9 +35,11 @@ public class StringPermutationRecursive {
 	 */
 	private static void permutation(String prefix, String str) {
 	    int n = str.length();
+	    // Base Condition
 	    if (n == 0) {
 	    	System.out.println(prefix);
 	    } else {
+	    	// Recursive Condition
 	    	for (int i = 0; i < n; i++) {
 	    		permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
 	    	}
