40ee99e13d245076c15963c8eb9e4843a9993138
Jigar Gosalia <jgosalia@paypal.com>
2018-04-12 18:05:02 -0700

    LAZY: string reversal

---
+++ src/string/StringReversal.java
@@ -0,0 +1,29 @@
+package string;
+
+import java.util.Arrays;
+
+public class StringReversal {
+
+	public static void main(String[] args) {
+		for (String word : Arrays.asList(null, "", "a", "ab", "abc", "hello how are you?")) {
+			System.out.printf("%-20s : %-20s : %-20s\n", word, reverse(word), reverseRecursive(word));
+		}
+	}
+
+	private static String reverseRecursive(String input) {
+		return ((input != null && input.length() > 0) ? reverse(input.substring(1)) + input.charAt(0) : input);
+	}
+
+	private static String reverse(String input) {
+		if (input != null && input.length() > 0) {
+			char[] characters = input.toCharArray();
+			for (int index = 0; index < input.length()/2; index++) {
+				char temp = characters[index];
+				characters[index] = characters[input.length() - 1 - index];
+				characters[input.length() - 1 - index] = temp;
+			}
+			return new String(characters);
+		}
+		return input;
+	}
+}
