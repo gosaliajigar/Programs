cb4c16b4721e53946eafe2a944ba988399a82305
Jigar Gosalia <jgosalia@paypal.com>
2018-05-15 14:04:58 -0700

    LAZY: add Dijkstra and Topological Sort

---
+++ src/graphs/algorithm/Dijkstra.java
@@ -0,0 +1,31 @@
+package graphs.algorithm;
+
+/**
+ * @author Jigar Gosalia
+ *
+ */
+public class Dijkstra {
+
+	// Dijkstra is Single Source shortest paths from a give source without negative paths
+	// Dijkstra is BFS without weights
+	//
+	// O(V)*EXTRACT-MIN + O(E)*DECREASE-KEY
+	// 
+	// d[s] = 0
+	// for each v in V-{s}
+	//  	do d[v] = Integer.MAX_VALUE (infinite)
+	// S = null
+	// Q = V (priority queue)
+	// while !Q.isEmpty()
+	//		do u = EXTRACT_MIN(Q)
+	//			S = S union {u}
+	//			for each v in Adj[u]
+	//				do if d[v] > d[u] + w(u, v)
+	//					then d[v] = d[u] + w(u, v)
+	//
+	//
+	// Array 			: O(V^2)
+	// Binary Heap		: O(E*logV)
+	// Fibonacci Heap	: O(E+VlogV)
+
+}
---
+++ src/graphs/algorithm/TopologicalSort.java
@@ -0,0 +1,17 @@
+package graphs.algorithm;
+
+/**
+ * @author Jigar Gosalia
+ *
+ */
+public class TopologicalSort {
+
+	// Topological Sort is implementation fo DFS
+	// 
+	// O(V+E)
+	//
+	// Call DFS to compute finish time f[v] for each vertex
+	// As each vertex is finished, insert it onto front of a linked list
+	// Return the linked list of vertices
+	// 
+}
