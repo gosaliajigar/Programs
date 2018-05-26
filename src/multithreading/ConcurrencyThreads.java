08fae936c4c3cb8ab6fbb806ff48e499543a9a50
Jigar Gosalia <jgosalia@paypal.com>
2018-05-26 14:40:20 -0700

    LAZY: combine threading examples

---
+++ src/multithreading/ConcurrencyThreads.java
@@ -0,0 +1,11 @@
+package multithreading;
+
+/**
+ * @author "Jigar Gosalia"
+ *
+ */
+public class ConcurrencyThreads {
+	public static void main(final String args[]) {
+	}
+}
+
--- src/multithreading/ThreadNRunnable.java
+++ src/multithreading/ThreadNRunnable.java
@@ -4,15 +4,17 @@ package multithreading;
  * @author "Jigar Gosalia"
  *
  */
-public class ByExtendingThread {
+public class ThreadNRunnable {
 
 	/**
 	 * @param args
 	 */
 	public static void main(final String args[]) {
 		new ThreadDemo("Thread-1").start();
-
 		new ThreadDemo("Thread-2").start();
+		new Thread(new RunnableDemo("Thread-3")).start();
+		new Thread(new RunnableDemo("Thread-4")).start();
+
 	}
 }
 
@@ -39,8 +41,38 @@ class ThreadDemo extends Thread {
 		try {
 			for (int i = 1000; i >= 100; i -= 100) {
 				System.out.println("Thread " + threadName + " - " + i);
-				// Let the thread sleep for a while.
-				Thread.sleep(10);
+			}
+		} catch (Exception e) {
+			System.out.println("Thread " + threadName + " interrupted.");
+		}
+		System.out.println("Thread " + threadName + " exiting.");
+	}
+
+}
+
+class RunnableDemo implements Runnable {
+
+	private String threadName;
+
+	/**
+	 * @param name
+	 */
+	RunnableDemo(final String name) {
+		threadName = name;
+		System.out.println("Creating " + threadName);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		System.out.println("  Running " + threadName);
+		try {
+			for (int i = 1000; i > 100; i -= 100) {
+				System.out.println("Thread " + threadName + ", " + i);
 			}
 		} catch (Exception e) {
 			System.out.println("Thread " + threadName + " interrupted.");
