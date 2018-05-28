fde9cc0a5615ee1b2d413a18a7e0e3b214a3b119
Jigar Gosalia <jgosalia@paypal.com>
2018-05-27 20:08:00 -0700

    LAZY: priorityqueue and blockingpriorityqueue

--- src/collections/C030PriorityQueue.java
+++ src/collections/C030PriorityQueue.java
@@ -1,9 +1,107 @@
 package collections;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
+ * PriorityQueue is an unbounded Queue implementation in Java, which is based on
+ * priority heap. PriorityQueue allows you to keep elements in a particular
+ * order, according to there natural order or custom order defined by Comparator
+ * interface in Java.<br>
+ * <br>
+ * 
+ * Iterator returned by PriorityQueue doesn't offer any ordering traversal
+ * guarantee.<br>
+ * <br>
+ * <br>
+ * 
+ * 		Summary of Queue methods <br>
+ * 			Throws exception 		Returns special value<br> 
+ * Insert\t 	add(e)					offer(e)<br> 
+ * Remove 		remove() 				poll() <br>
+ * Examine 		element() 				peek() <br>
+ * <br>
+ * 
+ * 
+ * Source:
+ * https://javarevisited.blogspot.com/2013/10/what-is-priorityqueue-data-structure-java-example-tutorial.html
+ * <br>
+ * <br>
+ * 
  * @author Jigar Gosalia
  *
  */
 public class C030PriorityQueue {
+
+	public static void main(String args[]) {
+		// [0-9] [A-Z] [a-z]
+		Queue<Item> items = new PriorityQueue<Item>();
+		items.addAll(Arrays.asList(new Item("iPhone", 900), new Item("iPad", 300), new Item("Xbox", 300),
+				new Item("iWatch", 300)));
+		System.out.println(items);
+		System.out.println();
+
+		System.out.println(items.peek() + " : " + items);
+		System.out.println();
+
+		System.out.println(items.poll() + " : " + items);
+		System.out.println();
+
+		// null elements not allowed in PriorityQueue - NullPointerException
+		// items.add(null);
+
+		items.offer(new Item("Pixel", 300));
+	}
+}
+
+class Item implements Comparable<Item> {
+
+	private String name;
+	private int price;
+
+	public Item(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Item other = (Item) obj;
+		if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+			return false;
+		}
+		if (this.price != other.price) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 5;
+		hash = 97 * hash + (this.name != null ? this.name.hashCode() : 0);
+		hash = 97 * hash + this.price;
+		return hash;
+	}
+
+	@Override
+	public int compareTo(Item i) {
+		if (this.name == i.name) {
+			return this.price - i.price;
+		}
+		return this.name.compareTo(i.name);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s: $%d", name, price);
+	}
 
 }
--- src/collections/C031PriorityBlockingQueue.java
+++ src/collections/C031PriorityBlockingQueue.java
@@ -1,5 +1,41 @@
 package collections;
 
+/**
+ * 
+ * An unbounded blocking queue that uses the same ordering rules as class 
+ * PriorityQueue and supplies blocking retrieval operations. While this queue 
+ * is logically unbounded, attempted additions may fail due to resource 
+ * exhaustion (causing OutOfMemoryError). This class does not permit null elements.
+ * A priority queue relying on natural ordering also does not permit insertion of 
+ * non-comparable objects (doing so results in ClassCastException) <br>
+ * <br>
+ * 
+ * A Queue that additionally supports operations that wait for the queue to
+ * become non-empty when retrieving an element, and wait for space to become
+ * available in the queue when storing an element. BlockingQueue methods come in
+ * four forms, with different ways of handling operations that cannot be
+ * satisfied immediately, but may be satisfied at some point in the future: one
+ * throws an exception, the second returns a special value (either null or
+ * false, depending on the operation), the third blocks the current thread
+ * indefinitely until the operation can succeed, and the fourth blocks for only
+ * a given maximum time limit before giving up. These methods are summarized in
+ * the following table: <br>
+ * 
+ * Summary of BlockingQueue methods<br>
+ * 			Throws exception 	Special value 		Blocks 			Times out<br>
+ * Insert 		add(e) 				offer(e) 		put(e) 		offer(e, time, unit)<br>
+ * Remove 		remove() 			poll() 			take() 		poll(time, unit) <br>
+ * Examine 		element() 			peek() 			N/A 			N/A<br>
+ * <br>
+ * <br>
+ * 
+ * @author Jigar Gosalia
+ *
+ */
 public class C031PriorityBlockingQueue {
 
+	public static void main(String[] args) {
+		// Check @link{BQMainClass.class}
+		// put and take
+	}
 }
